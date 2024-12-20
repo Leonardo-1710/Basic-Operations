272055985a758af52427b49567a59cab7dff9ec8
Leonardo-1710 <leonardoalvesalmeida1982@gmail.com>
2024-12-20 11:23:08 +0000

    Python

---
+++ OperacoesBasicas.py
@@ -0,0 +1,25 @@
+'''
+    Programador.....: (C) 2024 Leonardo Almeida
+    Data............: 20/12/2024
+    Observações.....: Operações básicas em Python
+'''
+
+n1 = 0
+n2 = 0
+soma = 0
+subtracao = 0
+divisao = 0
+multiplicacao = 0
+
+n1 = int(input("Introduza um nº inteiro:"))
+n2 = int(input("Introduza outro nº inteiro:"))
+
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+
+print("Soma:", soma)
+print("Subtracao:", subtracao)
+print("Divisao:", divisao)
+print("Multiplicacao:", multiplicacao)
